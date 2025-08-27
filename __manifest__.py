320051ccaad51d5cc7e5a940420fab772d369bbd
Chensz <chensz.hengki@gmail.com>
2025-08-27 23:39:09 +0800

    add odoo structure

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name':'Eff Product Catalog',
+    'version':'1.0',
+    'category': 'Catalog',
+    'author':'Chensz',
+    'website':'',
+    'summary':'Simple product and catalog management',
+    'description':'Manage product and catalog',
+
+    'depends':[
+        'base',
+        'website',
+        'product'
+    ],
+    'installable':True,
+    'application': True,
+}
